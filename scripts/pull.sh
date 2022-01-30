66e7681f0160211fcc9fe1303efcc3c1bf1c63af
Durvesh <durveshkulkarni55@gmail.com>
2022-01-30 18:01:55 +0530

    5

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -3,6 +3,6 @@
 mkdir /home/test1/Documents/exam
 cd /home/test1/Documents/exam
 git clone https://github.com/kulkarniDurvesh/devops-210940120097-.git
-cd ./devops-210940120097
+cd ./devops-210940120097-
 javac program.java
 java program
