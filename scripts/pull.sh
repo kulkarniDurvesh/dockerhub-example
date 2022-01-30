11e950b0a5c2b1adafa3930242f83728af0965b6
Durvesh <durveshkulkarni55@gmail.com>
2022-01-30 17:27:22 +0530

    2

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf /home/exam
-mkdir /home/exam
+sudo rm -rf /home/exam
+sudo mkdir /home/exam
 cd /home/exam
 git clone https://github.com/kulkarniDurvesh/devops-210940120097-.git
 cd ./devops-210940120097
